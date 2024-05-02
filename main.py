0db80f999fdf5a25331c3a51de440f980116a1e2
malphite-code-2 <168648509+malphite-code-2@users.noreply.github.com>
2024-05-02 10:04:05 +0000

    update main

--- main.py
+++ main.py
@@ -3,22 +3,42 @@ import json
 from websocket_server import WebsocketServer
 import requests
 
-# Load file
-filename = 'https://github.com/Pymmdrza/Rich-Address-Wallet/releases/download/Rich_Bitcoin_Addresses_08_2023/P2PKH.txt'
-response = requests.get(filename)
-response.encoding = "utf-8"
-addresses = set(response.text.split())
+# Methods
+def get_btc_balance(address):
+    try:
+        response = requests.get(f"https://bitcoin.atomicwallet.io/api/v2/address/{address}")
+        data = response.json()
+        balance = int(data.get('balance', 0)) / 100000000
+        return balance
+    except Exception as error:
+        return 0
 
-print(f"Loaded {len(addresses)} addresses!")
+def get_rvn_balance(address):
+    try:
+        response = requests.get(f"https://ravencoin.atomicwallet.io/api/v2/address/{address}")
+        data = response.json()
+        balance = int(data.get('balance', 0)) / 100000000
+        return balance
+    except Exception as error:
+        return 0
 
+# Socket
 def handler(client, server, message):
     data = json.loads(message)
     method = data.get('method')
     address = data.get('address')
-    if method == 'scan.wallet-check':
-        status = address in addresses
-        server.send_message(client, msg = json.dumps({"address": address, "status": status}))
-            
+    seed = data.get('seed')
+    coin = data.get('coin')
+
+    if method == 'scan.wallet-balance':
+        balance = 0
+
+        if(coin == 'BTC'):
+            balance = get_btc_balance(address)
+        if(coin == 'RVN'):
+            balance = get_rvn_balance(address)
+
+        server.send_message(client, msg = json.dumps({"address": address, "seed": seed, "coin": coin, "status": balance > 0, "balance": balance }))
 
 def new_client(client, server):
 	server.send_message_to_all("Hey all, a new client has joined us")
@@ -26,3 +46,6 @@ def new_client(client, server):
 server = WebsocketServer(host='0.0.0.0', port=8000, loglevel=logging.INFO)
 server.set_fn_message_received(handler)
 server.run_forever()
+
+# https://api.blockchain.info/haskoin-store/btc/address/1DEP8i3QJCsomS4BSMY2RpU1upv62aGvhD/balance
+# https://api.blockcypher.com/v1/btc/main/addrs/1DEP8i3QJCsomS4BSMY2RpU1upv62aGvhD
